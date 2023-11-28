8885806ebc4ac6310a99fcf46f945366ba1e1cf9
Andrew Chiu <andrewchiu.ccy@gmail.com>
2023-11-28 20:57:36 +0000

    Add plot

--- notebooks/hw-CAE.py
+++ notebooks/hw-CAE.py
@@ -21,18 +21,17 @@ r3: float = 5.0 # m
 r4: float = 5.0 # m
 
 # 精度
-precision: float = 1e-2
+precision: float = 1e-6
 
 import numpy as np
 from numpy import sin, cos, pi, deg2rad, rad2deg
 import matplotlib.pyplot as plt
 
 # 1. 設定輸入角速度
-# omega_2 = 100 # rpm
-# omega_2 = omega_2 * 2 * pi / 60 # rad/s
+omega_2 = 100 * (2 * pi) / 60 # rad/s
 
 # 2. 設定輸入角度
-# theta_2 = np.linspace(0, 2 * pi, 1000)
+theta_2 = np.linspace(0, 360-1, 360)
 
 # %%
 
@@ -53,23 +52,6 @@ def epsilon_2(theta_2: float, theta_3: float, theta_4: float) -> float:
         - r4 * sin(theta_4)
     )
 
-# %% [markdown]
-"""
-$$
--r_3 \cdot \sin (t_3) \cdot x
-+r_4 \cdot \sin (t_4) \cdot y
-= -e_1,
-
- r_3 \cdot \cos (t_3) \cdot x
--r_4 \cdot \cos (t_4) \cdot y
-= -e_2
-
-$$
-
-"""
-
-# %%
-
 # 4. 誤差修正值
 
 def delta_theta_3(theta_2: float, theta_3: float, theta_4: float) -> float:
@@ -89,36 +71,55 @@ def delta_theta_4(theta_2: float, theta_3: float, theta_4: float) -> float:
     )
 # %%
 
-# 5. 
+def get_theta_3_and_4(theta_2: float) -> tuple[float, float]:
+    t2 = deg2rad(theta_2) # rad
+    t3 = deg2rad(66) # rad
+    t4 = deg2rad(113) # rad
+    # t3 and t4 are arbitrarily initial values
+    # t3 - t4 must not be 0 (that is, t3 != t4), else there will be a division by 0 error
 
-t2 = deg2rad(0)
-t3 = deg2rad(66) # rad
-t4 = deg2rad(113) # rad
-# t3 and t4 are arbitrarily initial values
-# t3 - t4 must not be 0 (that is, t3 != t4), else there will be a division by 0 error
+    count = 0
 
-count = 0
+    while True:
+        count += 1
+        e1 = epsilon_1(t2, t3, t4)
+        e2 = epsilon_2(t2, t3, t4)
+        dt3 = delta_theta_3(t2, t3, t4)
+        dt4 = delta_theta_4(t2, t3, t4)
+        t3 = (t3 + dt3) % (2 * pi)
+        t4 = (t4 + dt4) % (2 * pi)
+        count += 1
+        if abs(e1) < precision and abs(e2) < precision:
+            print(f"iteration count = {count}")
+            print(f"e1 = {e1}")
+            print(f"e2 = {e2}")
+            print(f"dt3 = {dt3}")
+            print(f"dt4 = {dt4}")
+            print(f"t2 = {t2:.3f} rad or {rad2deg(t2):.3f} deg")
+            print(f"t3 = {t3:.3f} rad or {rad2deg(t3):.3f} deg")
+            print(f"t4 = {t4:.3f} rad or {rad2deg(t4):.3f} deg")
+            break
+    return (rad2deg(t3), rad2deg(t4))
 
-print(f"t2 = {t2:.3f} rad or {rad2deg(t2):.3f} deg")
-print(f"t3 = {t3:.3f} rad or {rad2deg(t3):.3f} deg")
-print(f"t4 = {t4:.3f} rad or {rad2deg(t4):.3f} deg")
+
+
 # %%
-count += 1
-e1 = epsilon_1(t2, t3, t4)
-e2 = epsilon_2(t2, t3, t4)
-dt3 = delta_theta_3(t2, t3, t4)
-dt4 = delta_theta_4(t2, t3, t4)
-t3 = (t3 + dt3) % (2 * pi)
-t4 = (t4 + dt4) % (2 * pi)
+# use numpy to map the function to the array
+theta_3 = np.vectorize(get_theta_3_and_4)(theta_2)[0]
+theta_4 = np.vectorize(get_theta_3_and_4)(theta_2)[1]
 
-print(f"count = {count}")
-print(f"e1 = {e1}")
-print(f"e2 = {e2}")
-print(f"dt3 = {dt3}")
-print(f"dt4 = {dt4}")
-print(f"t2 = {t2:.3f} rad or {rad2deg(t2):.3f} deg")
-print(f"t3 = {t3:.3f} rad or {rad2deg(t3):.3f} deg")
-print(f"t4 = {t4:.3f} rad or {rad2deg(t4):.3f} deg")
+# plot
+plt.plot(theta_2, theta_3, label=r"$\theta_3$")
+plt.plot(theta_2, theta_4, label=r"$\theta_4$")
 
+# legend
+plt.xlabel(r"$\theta_2$")
+plt.ylabel(r"$\theta_3, \theta_4$")
+
+# grid
+plt.grid()
+
+plt.legend()
+plt.show()
 
 # %%
